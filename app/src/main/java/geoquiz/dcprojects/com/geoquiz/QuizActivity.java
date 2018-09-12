24add46d9de1d28cf1006cfad515a60b55212073
Dillon Carns <carnsds@appstate.edu>
2018-09-12 16:07:32 -0400

    Fixed some of the questions, added check answer method.

--- app/src/main/java/geoquiz/dcprojects/com/geoquiz/QuizActivity.java
+++ app/src/main/java/geoquiz/dcprojects/com/geoquiz/QuizActivity.java
@@ -17,8 +17,8 @@ public class QuizActivity extends AppCompatActivity {
     private Question[] mQuestionBank = new Question[] {
             new Question(R.string.question_australia, true),
             new Question(R.string.question_oceans, true),
-            new Question(R.string.question_mideast, true),
-            new Question(R.string.question_africa, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
             new Question(R.string.question_americas, true),
             new Question(R.string.question_asia, true),
     };
@@ -38,6 +38,7 @@ public class QuizActivity extends AppCompatActivity {
         updateQuestion();
         mTrueButton = (Button) findViewById(R.id.true_button);
         mFalseButton = (Button) findViewById(R.id.false_button);
+        mNextButton = (Button) findViewById(R.id.next_button);
 
         initButtonFunctions();
     }
@@ -46,31 +47,45 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
-                        R.string.correct_toast,
-                        Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
             }
         });
 
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
-                        R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
+
 
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-            }
+                updateQuestion();
+                }
         });
+
     }
 
     private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getTextresId();
         mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userAnswer) {
+
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+        int messageResId = 0;
+
+        if (userAnswer == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
 }
