ac7f7618b29cf71c953048d85d4866e607cddd78
Dillon Carns <carnsds@appstate.edu>
2018-09-12 15:52:48 -0400

    Added functionality to Next button and encapsulated some methods.

--- app/src/main/java/geoquiz/dcprojects/com/geoquiz/QuizActivity.java
+++ app/src/main/java/geoquiz/dcprojects/com/geoquiz/QuizActivity.java
@@ -2,28 +2,47 @@ package geoquiz.dcprojects.com.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.transition.TransitionManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_australia, true),
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, true),
+            new Question(R.string.question_africa, true),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true),
+    };
+
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        init();
+    }
+
+    private void init() {
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        updateQuestion();
         mTrueButton = (Button) findViewById(R.id.true_button);
         mFalseButton = (Button) findViewById(R.id.false_button);
 
-        initButtons();
+        initButtonFunctions();
     }
 
-    private void initButtons() {
+    private void initButtonFunctions() {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,5 +60,17 @@ public class QuizActivity extends AppCompatActivity {
                         Toast.LENGTH_SHORT).show();
             }
         });
+
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+            }
+        });
+    }
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextresId();
+        mQuestionTextView.setText(question);
     }
 }
